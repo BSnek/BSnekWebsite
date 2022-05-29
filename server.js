6e3f87acecbfa7792855cacacc075ee147807682
Kazu Tan <KazuTanzx@gmail.com>
2022-05-29 16:13:21 +0800

    Making sure env listen to port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const port = 3000
+
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
